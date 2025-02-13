d015ce4390f1b4b4a84e9521de709b312ec612da
Mauricio2085 <maoca2085@gmail.com>
2025-02-13 16:14:16 -0500

    Actualizar la cadena de conexión en el pool de PostgreSQL para incluir credenciales de usuario y contraseña

--- libs/postgres.pool.js
+++ libs/postgres.pool.js
@@ -1,19 +1,16 @@
 const { Pool } = require('pg');
+
 const { config } = require('./../config/config');
 
-// const USER = encodeURIComponent(config.dbUser);
-// const PASSWORD = encodeURIComponent(config.dbPassword);
-// const URI =
-//   config.dbUrl ||
-//   `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
-// console.log(URI);
-const connectionString = config.dbPublicUrl || config.dbUrl;
-console.log(connectionString);
+const USER = encodeURIComponent(config.dbUser);
+const PASSWORD = encodeURIComponent(config.dbPassword);
+const URI =
+  config.dbUrl ||
+  `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+console.log(URI);
 const pool = new Pool({
-  connectionString,
+  connectionString: URI,
   ssl: { rejectUnauthorized: false },
 });
 
 module.exports = pool;
-
-// postgresql://postgres:tZDcqOPLXAlnNjpUelNdrlOVMsWoMVrK@postgres.railway.internal:5432/railway
