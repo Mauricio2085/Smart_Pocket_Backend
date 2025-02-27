0876e31277937512a69c79b5931c16418989401d
Mauricio2085 <maoca2085@gmail.com>
2025-02-27 16:19:30 -0500

    Actualizar configuración de CORS para permitir múltiples orígenes y mejorar la experiencia de desarrollo

--- index.js
+++ index.js
@@ -7,13 +7,21 @@ const errorHandler = require('./middleware/errorHandler');
 const urlProduction = 'https://smart-pocket-v1.vercel.app';
 const urllocal = 'http://localhost:3000';
 
-app.use(
-  cors({
-    origin: urlProduction,
-    methods: 'GET,POST,PUT,DELETE',
-    allowedHeaders: 'Content-Type,Authorization',
-  })
-);
+const urlList = [urlProduction, urllocal];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || urlList.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: 'GET,POST,PUT,DELETE',
+  allowedHeaders: 'Content-Type,Authorization',
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.listen(port, () => {
